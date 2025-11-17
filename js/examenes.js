f1416242c9c23372ab255c45c8aa299d000a9d2b
Carpiox <Carpiomaster@gmail.com>
2025-11-17 13:13:40 +0100

    Logica implementada examenes.html, pequeñas mejoras globales

---
+++ js/examenes.js
@@ -0,0 +1,19 @@
+let boton = document.getElementById("boton");
+let resultado = document.getElementById("resultado");
+
+boton.addEventListener("click",()=>{
+    let asignatura = document.getElementById("lista-asignatura");
+    let comunidad = document.getElementById("lista-comunidad");
+    let anyo = document.getElementById("anyo");
+
+    const base = `https://calculadoraevau.netlify.app/Examenes/${asignatura}/${comunidad}/${anyo}/ordinaria.pdf`;
+
+
+
+    
+
+
+
+
+
+})
